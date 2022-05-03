462150ec22cd23b6fc45c286c966e03da3b06933
gautam6023 <gautamgohil0@gmail.com>
2022-05-03 21:51:41 +0530

    package

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
-import Navbar from "./Components/Navbar";
+import { Navbar, SubNav } from "./Components/Navbar";
+import { Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <SubNav />
     </div>
   );
 }
