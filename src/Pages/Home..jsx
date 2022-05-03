423497c83fe9ecc2a9830d191c201adb83a71cf0
gautam6023 <gautamgohil0@gmail.com>
2022-05-03 16:41:02 +0530

    updated

---
+++ src/Pages/Home..jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Components/Navbar'
+import { Link } from 'react-router-dom'
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Link >About</Link>
+    </div>
+  )
+}
+
+export default Home.
