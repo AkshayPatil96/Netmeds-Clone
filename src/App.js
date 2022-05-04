e1cd730409f04fe29ae191a5d016634499634803
kothadiyasagar <sagarkothadiya98@gmail.com>
2022-05-04 10:27:27 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+<<<<<<< Updated upstream
 
 import { Navbar, SubNav } from "./Components/Navbar";
 import { Routes } from "react-router-dom";
@@ -9,6 +10,14 @@ function App() {
     <div className="App">
       <Navbar />
       <SubNav />
+=======
+import Navbar from "./Components/Navbar";
+import Cart from "./Pages/Cart";
+function App() {
+  return (
+    <div>
+      <Cart/>
+>>>>>>> Stashed changes
     </div>
   );
 }
