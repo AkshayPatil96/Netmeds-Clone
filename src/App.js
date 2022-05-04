53bc240e5b11054c368217e8f62ec9abad85fa96
gautam6023 <gautamgohil0@gmail.com>
2022-05-04 14:01:23 +0530

    changed

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+
+import Home from "./Pages/Home";
+
+import { Navbar, SubNav } from "./Components/Navbar";
+import { Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <SubNav />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
