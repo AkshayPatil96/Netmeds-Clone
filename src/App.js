006ee6607d199e17f2700eccc6a9b6c18ff9adea
Akshay Patil <53942517+AkshayPatil96@users.noreply.github.com>
2022-05-04 13:54:09 +0530

    updated App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Home from "./Pages/Home";
+import { Navbar, SubNav } from "./Components/Navbar";
+
+const App = () => {
+    return (
+        <div>
+            <Navbar />
+            <SubNav />
+            <Home />
+        </div>
+    );
+};
+
+export default App;
