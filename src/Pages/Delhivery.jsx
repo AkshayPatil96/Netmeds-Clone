4483dca335cf6b5f06f6252cb2bbd2169fd91bc8
Gaurav Kumar <gauravkr2179@gmail.com>
2022-05-08 11:41:36 +0530

    payment

--- src/Pages/Delhivery.jsx
+++ src/Pages/Delhivery.jsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { Navbar } from "../Components/Navbar";
 
 const Delhivery = () => {
+  const navigate = useNavigate()
   return (
     <div>
-      <Navbar />
       <div>
         <h1>Your Product successfully order</h1>
-        <button>Go to Home</button>
+        <button onClick={()=>navigate("/")}>Go to Home</button>
       </div>
     </div>
   );
