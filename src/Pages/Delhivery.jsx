7714f4a23627aba2c172fff4cea3755127c81a6d
Gaurav Kumar <gauravkr2179@gmail.com>
2022-05-09 09:50:00 +0530

    delhivary

--- src/Pages/Delhivery.jsx
+++ src/Pages/Delhivery.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Navbar } from "../Components/Navbar";
+import styled from 'styled-components'
+
+const Wrapper =styled.div`
+  padding:25px;
+  text-align:center;
+`
+const Button = styled.button`
+width:150px;
+height: 40px;
+border: none;
+background-color: #24aeb1;
+border-radius: 5px;
+color: #fff;
+font-size: 15px;
+cursor: pointer;
+`
+
 
 const Delhivery = () => {
   const navigate = useNavigate()
   return (
     <div>
-      <div>
+      <Wrapper>
         <h1>Your Product successfully order</h1>
-        <button onClick={()=>navigate("/")}>Go to Home</button>
-      </div>
+        <Button onClick={()=>navigate("/")}>Go to Home</Button>
+      </Wrapper>
     </div>
   );
 };
